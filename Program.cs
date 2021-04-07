64c6c2376a813eeb7b7679f79920e09e612f0b4b
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-04-07 13:12:29 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Personas
+{
+    class Persona{
+        string nombre;
+        public string Nombre { get{return nombre;} set{nombre = value;} }
+        string[] lapersona = new string[3];
+
+        public Persona(){
+            for(int i = 0; i < 3;i++){
+                Console.WriteLine("Ingrese un nombre: ");
+                nombre = Console.ReadLine();
+                lapersona[i] = nombre;
+            }
+        }
+        public void Mostrar(){
+            for(int i = 0; i < 3; i++){
+            Console.WriteLine("\nEl nombre del apersona es {0}", lapersona[i]);
+            }
+        }
+        ~Persona(){
+            //limpiar
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.Mostrar();
+        }
+    }
+}
